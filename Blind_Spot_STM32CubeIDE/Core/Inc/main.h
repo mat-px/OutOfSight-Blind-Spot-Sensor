dbba76af4e022f1193cccea4e6d6c07b2e7402db
mat <madprzez@gmail.com>
2023-02-20 17:10:55 -0600

    fix

--- Blind_Spot_STM32CubeIDE/Core/Inc/main.h
+++ Blind_Spot_STM32CubeIDE/Core/Inc/main.h
@@ -28,8 +28,6 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l0xx_hal.h"
-#include "stm32l0xx.h"
-#include "stm32l0xx_it.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
